ad61fb2ab9b351fdf13ef56955b000a0c30ea71d
tenzin <chonden808@gmail.com>
2026-08-31 19:11:03 +0900

    random walk class done

---
+++ rw_visual.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+while True:
+# Make a random walk
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+
+    # Plot the points in the walk
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    plt.title('Pollen grain path on the surface of a drop of water')
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
